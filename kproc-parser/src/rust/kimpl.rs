984b36c1a87819d6f815583a05af700b3b2381ae
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2022-12-20 00:24:08 +0100

    feat(kparser): parsing the full impl block
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- kproc-parser/src/rust/kimpl.rs
+++ kproc-parser/src/rust/kimpl.rs
@@ -4,6 +4,7 @@ use crate::diagnostic::{KDiagnInfo, KDiagnostic};
 use crate::eassert_eq;
 use crate::kparser::KParserTracer;
 use crate::kproc_macros::KTokenStream;
+use crate::rust::ast_nodes::ImplToken;
 use crate::rust::core::parse_decl_generics_and_lifetime;
 
 pub fn parse_impl<'c>(ast: &'c mut KTokenStream, tracer: &dyn KParserTracer) -> RustAST {
@@ -15,12 +16,32 @@ pub fn parse_impl<'c>(ast: &'c mut KTokenStream, tracer: &dyn KParserTracer) -> 
         format!("expected `impl` found `{}`", impl_tok.to_string())
     );
     let _ = parse_decl_generics_and_lifetime(ast, tracer);
-    let for_tok = ast.advance().to_owned();
-    eassert_eq!(
-        "for",
-        for_tok.to_string(),
-        for_tok,
-        format!("expected `for` found `{}`", for_tok.to_owned())
-    );
-    todo!()
+    let name = ast.advance().to_owned();
+    let _for_ty = if ast.match_tok("for") {
+        // FIXME: parsing the generic and lifetime usage
+        let for_tok = ast.advance().to_owned();
+        eassert_eq!(
+            "for",
+            for_tok.to_string(),
+            for_tok,
+            format!("expected `for` found `{}`", for_tok.to_owned())
+        );
+        Some(ast.advance())
+    } else {
+        None
+    };
+
+    // store the raw content of the block because there
+    // if the user want parse it,
+    // it has all the necessary tools for parse it.
+    let impl_block = ast.advance().to_owned();
+    let impl_ast = ImplToken {
+        decl_lifetims: None,
+        decl_generics: None,
+        name,
+        // FIXME: make an abstraction for this kind of type
+        for_ty: None,
+        impl_block,
+    };
+    RustAST::Impl(impl_ast)
 }
