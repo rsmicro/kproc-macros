71541becddfee6bfde396448a9031d1ae3286504
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2022-12-20 00:07:12 +0100

    feat(kparser): move some code to core
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- kproc-parser/src/rust/kimpl.rs
+++ kproc-parser/src/rust/kimpl.rs
@@ -4,8 +4,9 @@ use crate::diagnostic::{KDiagnInfo, KDiagnostic};
 use crate::eassert_eq;
 use crate::kparser::KParserTracer;
 use crate::kproc_macros::KTokenStream;
+use crate::rust::core::parse_decl_generics_and_lifetime;
 
-pub fn parse_impl<'c>(ast: &'c mut KTokenStream, _: &dyn KParserTracer) -> RustAST {
+pub fn parse_impl<'c>(ast: &'c mut KTokenStream, tracer: &dyn KParserTracer) -> RustAST {
     let impl_tok = ast.advance().to_owned();
     eassert_eq!(
         "impl",
@@ -13,6 +14,13 @@ pub fn parse_impl<'c>(ast: &'c mut KTokenStream, _: &dyn KParserTracer) -> RustA
         impl_tok,
         format!("expected `impl` found `{}`", impl_tok.to_string())
     );
-
+    let _ = parse_decl_generics_and_lifetime(ast, tracer);
+    let for_tok = ast.advance().to_owned();
+    eassert_eq!(
+        "for",
+        for_tok.to_string(),
+        for_tok,
+        format!("expected `for` found `{}`", for_tok.to_owned())
+    );
     todo!()
 }
